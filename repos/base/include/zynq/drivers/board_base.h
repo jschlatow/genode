9401c1f8839b2359ba54f252225a83b758f24356
Martin Stein <martin.stein@genode-labs.com>
2015-06-23 12:50:33 +0200

    Fixup "base-hw: Add basic Zynq-7000 support (QEMU)"
    Don't declare Board_base internals inside the namespace of the class.
    Saves tabs.
    
    Ref #1599

--- repos/base/include/zynq/drivers/board_base.h
+++ repos/base/include/zynq/drivers/board_base.h
@@ -16,69 +16,68 @@
 #ifndef _INCLUDE__DRIVERS__BOARD_BASE_H_
 #define _INCLUDE__DRIVERS__BOARD_BASE_H_
 
-namespace Genode
+namespace Genode { struct Board_base; }
+
+/**
+ * Base driver for the Zynq platform
+ */
+struct Genode::Board_base
 {
-	/**
-	 * Base driver for the Zynq platform
-	 */
-	struct Board_base
+	enum
 	{
-		enum
-		{
-			/* device IO memory */
-			MMIO_0_BASE    = 0xe0000000, /* IOP devices */
-			MMIO_0_SIZE    = 0x10000000,
-			MMIO_1_BASE    = 0xF8000000, /* Programmable register via APB */
-			MMIO_1_SIZE    = 0x02000000,
-			QSPI_MMIO_BASE = 0xFC000000, /* Quad-SPI */
-			QSPI_MMIO_SIZE = 0x01000000,
-			OCM_MMIO_BASE  = 0xFFFC0000, /* OCM upper address range */
-			OCM_MMIO_SIZE  = 0x00040000,
+		/* device IO memory */
+		MMIO_0_BASE    = 0xe0000000, /* IOP devices */
+		MMIO_0_SIZE    = 0x10000000,
+		MMIO_1_BASE    = 0xF8000000, /* Programmable register via APB */
+		MMIO_1_SIZE    = 0x02000000,
+		QSPI_MMIO_BASE = 0xFC000000, /* Quad-SPI */
+		QSPI_MMIO_SIZE = 0x01000000,
+		OCM_MMIO_BASE  = 0xFFFC0000, /* OCM upper address range */
+		OCM_MMIO_SIZE  = 0x00040000,
 
-			/* normal RAM */
-			RAM_0_BASE = 0x00000000,
-			RAM_0_SIZE = 0x40000000, /* 1GiB */
+		/* normal RAM */
+		RAM_0_BASE = 0x00000000,
+		RAM_0_SIZE = 0x40000000, /* 1GiB */
 
-			/* AXI */
-			AXI_0_MMIO_BASE = 0x40000000, /* PL AXI Slave port #0 */
-			AXI_0_MMIO_SIZE = 0x40000000,
-			AXI_1_MMIO_BASE = 0x80000000, /* PL AXI Slave port #1 */
-			AXI_1_MMIO_SIZE = 0x40000000,
+		/* AXI */
+		AXI_0_MMIO_BASE = 0x40000000, /* PL AXI Slave port #0 */
+		AXI_0_MMIO_SIZE = 0x40000000,
+		AXI_1_MMIO_BASE = 0x80000000, /* PL AXI Slave port #1 */
+		AXI_1_MMIO_SIZE = 0x40000000,
 
-			/* clocks (assuming 6:2:1 mode) */
-			CPU_1X_CLOCK   = 111111115,
-			CPU_6X4X_CLOCK = 6*CPU_1X_CLOCK,
+		/* clocks (assuming 6:2:1 mode) */
+		CPU_1X_CLOCK   = 111111115,
+		CPU_6X4X_CLOCK = 6*CPU_1X_CLOCK,
 
-			/* UART controllers */
-			UART_0_MMIO_BASE = MMIO_0_BASE,
-			UART_SIZE        = 0x1000,
-			UART_CLOCK       = 50*1000*1000,
+		/* UART controllers */
+		UART_0_MMIO_BASE = MMIO_0_BASE,
+		UART_SIZE        = 0x1000,
+		UART_CLOCK       = 50*1000*1000,
 
-			/* CPU */
-			CORTEX_A9_PRIVATE_MEM_BASE  = 0xf8f00000,
-			CORTEX_A9_PRIVATE_MEM_SIZE  = 0x00002000,
-			CORTEX_A9_CLOCK             = CPU_6X4X_CLOCK,
-			CORTEX_A9_PRIVATE_TIMER_CLK = CORTEX_A9_CLOCK, /* FIXME not exactly sure about this */
+		/* CPU */
+		CORTEX_A9_PRIVATE_MEM_BASE  = 0xf8f00000,
+		CORTEX_A9_PRIVATE_MEM_SIZE  = 0x00002000,
+		CORTEX_A9_CLOCK             = CPU_6X4X_CLOCK,
+		CORTEX_A9_PRIVATE_TIMER_CLK = CORTEX_A9_CLOCK, /* FIXME not sure */
 
-			/* CPU cache */
-			PL310_MMIO_BASE      = MMIO_1_BASE + 0xF02000,
-			PL310_MMIO_SIZE      = 0x1000,
-			CACHE_LINE_SIZE_LOG2 = 2, /* FIXME get correct value from board spec */
+		/* CPU cache */
+		PL310_MMIO_BASE      = MMIO_1_BASE + 0xF02000,
+		PL310_MMIO_SIZE      = 0x1000,
+		CACHE_LINE_SIZE_LOG2 = 2, /* FIXME get correct value from board spec */
 
-			/* TTC (triple timer counter) */
-			TTC0_MMIO_BASE = MMIO_1_BASE + 0x1000,
-			TTC0_MMIO_SIZE = 0xfff,
-			TTC0_IRQ_0     = 42,
+		/* TTC (triple timer counter) */
+		TTC0_MMIO_BASE = MMIO_1_BASE + 0x1000,
+		TTC0_MMIO_SIZE = 0xfff,
+		TTC0_IRQ_0     = 42,
 
-			/* Ethernet MAC PS */
-			EMAC_0_MMIO_BASE   = 0xE000B000,
-			EMAC_0_MMIO_SIZE   = 0x1000,
-			EMAC_0_IRQ         = 54,
+		/* Ethernet MAC PS */
+		EMAC_0_MMIO_BASE   = 0xE000B000,
+		EMAC_0_MMIO_SIZE   = 0x1000,
+		EMAC_0_IRQ         = 54,
 
-			/* wether board provides security extension */
-			SECURITY_EXTENSION = 0,
-		};
+		/* wether board provides security extension */
+		SECURITY_EXTENSION = 0,
 	};
-}
+};
 
 #endif /* _INCLUDE__DRIVERS__BOARD_BASE_H_ */
