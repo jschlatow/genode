8d1133e13447358ebd0dd0513c47b51f7849f719
Martin Stein <martin.stein@genode-labs.com>
2015-06-23 15:10:58 +0200

    hw: extract generic parts of PL310 to extra header
    hw_panda and hw_zynq both use the ARM PL310 L2 Cache controller. Zynq uses it
    in a complete generic way while Panda needs some additional interplay with
    the trustzone hypervisor. Thus, both boards use the base class Arm::Pl310 from
    the generic header spec/arm/pl310.h to implement there specific derivate
    Genode::Pl310.
    
    Ref #1599

--- repos/base-hw/src/core/include/spec/zynq/board.h
+++ repos/base-hw/src/core/include/spec/zynq/board.h
@@ -2,6 +2,7 @@
  * \brief  Board driver for core on Zynq
  * \author Johannes Schlatow
  * \author Stefan Kalkowski
+ * \author Martin Stein
  * \date   2014-06-02
  */
 
@@ -16,8 +17,8 @@
 #define _BOARD_H_
 
 /* core includes */
-#include <util/mmio.h>
 #include <spec/cortex_a9/board_support.h>
+#include <spec/arm/pl310.h>
 
 namespace Genode
 {
@@ -28,70 +29,11 @@ namespace Genode
 /**
  * L2 outer cache controller
  */
-class Genode::Pl310 : Genode::Mmio {
-
-	private:
-
-		struct Control : Register <0x100, 32>
-		{
-			struct Enable : Bitfield<0,1> {};
-		};
-
-		struct Aux : Register<0x104, 32>
-		{
-			struct Associativity  : Bitfield<16,1> { };
-			struct Way_size       : Bitfield<17,3> { };
-			struct Share_override : Bitfield<22,1> { };
-			struct Reserved       : Bitfield<25,1> { };
-			struct Ns_lockdown    : Bitfield<26,1> { };
-			struct Ns_irq_ctrl    : Bitfield<27,1> { };
-			struct Data_prefetch  : Bitfield<28,1> { };
-			struct Inst_prefetch  : Bitfield<29,1> { };
-			struct Early_bresp    : Bitfield<30,1> { };
-
-			static access_t init_value()
-			{
-				access_t v = 0;
-				Associativity::set(v, 1);
-				Way_size::set(v, 3);
-				Share_override::set(v, 1);
-				Reserved::set(v, 1);
-				Ns_lockdown::set(v, 1);
-				Ns_irq_ctrl::set(v, 1);
-				Data_prefetch::set(v, 1);
-				Inst_prefetch::set(v, 1);
-				Early_bresp::set(v, 1);
-				return v;
-			}
-		};
-
-		struct Irq_mask                : Register <0x214, 32> { };
-		struct Irq_clear               : Register <0x220, 32> { };
-		struct Cache_sync              : Register <0x730, 32> { };
-		struct Invalidate_by_way       : Register <0x77c, 32> { };
-		struct Clean_invalidate_by_way : Register <0x7fc, 32> { };
-
-		inline void _sync() { while (read<Cache_sync>()) ; }
-
+class Genode::Pl310 : public Arm::Pl310
+{
 	public:
 
-		void invalidate()
-		{
-			write<Invalidate_by_way>((1 << 16) - 1);
-			_sync();
-		}
-
-		void flush()
-		{
-			write<Clean_invalidate_by_way>((1 << 16) - 1);
-			_sync();
-		}
-
-		Pl310(Genode::addr_t const base) : Mmio(base)
-		{
-			write<Irq_mask>(0);
-			write<Irq_clear>(~0);
-		}
+		Pl310(addr_t const base) : Arm::Pl310(base) { _init(); }
 };
 
 /**
