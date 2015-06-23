c428f4b15e5093e43166c19fb8b95e862ab036f1
Martin Stein <martin.stein@genode-labs.com>
2015-06-23 16:38:35 +0200

    Fixup "hw_zynq: Add platform timer"
    Get rid of ttc_base.h. We don't need the base driver parts in an extra class
    as we don't use the TTC also in the kernel as, e.g., with the SP804.
    
    Ref #1599

--- repos/os/src/drivers/timer/hw/zynq/platform_timer_base.h
+++ repos/os/src/drivers/timer/hw/zynq/platform_timer_base.h
@@ -11,23 +11,57 @@
  * under the terms of the GNU General Public License version 2.
  */
 
-#ifndef _OS__SRC__DRIVERS__TIMER__HW__ZYNQ__PLATFORM_TIMER_BASE_H_
-#define _OS__SRC__DRIVERS__TIMER__HW__ZYNQ__PLATFORM_TIMER_BASE_H_
+#ifndef _HW__ZYNQ__PLATFORM_TIMER_BASE_H_
+#define _HW__ZYNQ__PLATFORM_TIMER_BASE_H_
 
 /* Genode includes */
 #include <io_mem_session/connection.h>
 #include <util/mmio.h>
-#include <drivers/timer/ttc_base.h>
 #include <drivers/board_base.h>
 
 /**
  * Platform-timer base specific for base-hw and zynq
- * (see Xilinx ug585 "Zynq 7000 Technical Reference Manual")
+ *
+ * Uses the internal timer 0 of the TTC. For more details, see Xilinx ug585
+ * "Zynq 7000 Technical Reference Manual".
  */
-class Platform_timer_base :
+class Platform_timer_base
+:
 	public Genode::Io_mem_connection,
-	public Genode::Ttc_base
+	public Genode::Mmio
 {
+	private:
+
+		enum { TICS_PER_US = Genode::Board_base::CPU_1X_CLOCK / 1000 / 1000 };
+
+		/**
+		 * Counter control register
+		 */
+		struct Control : Register<0x0c, 8>
+		{
+			struct Disable    : Bitfield<0, 1> { };
+			struct Mode       : Bitfield<1, 1> { enum { INTERVAL = 1 }; };
+			struct Decrement  : Bitfield<2, 1> { };
+			struct Wave_en    : Bitfield<5, 1> { };
+		};
+
+		/**
+		 * Counter value
+		 */
+		struct Value    : Register<0x18, 16> { };
+		struct Interval : Register<0x24, 16> { };
+		struct Match1   : Register<0x30, 16> { };
+		struct Match2   : Register<0x3c, 16> { };
+		struct Match3   : Register<0x48, 16> { };
+		struct Irq      : Register<0x54,  8> { };
+		struct Irqen    : Register<0x60,  8> { };
+
+		void _disable()
+		{
+			write<Control::Disable>(0);
+			read<Irq>();
+		}
+
 	public:
 
 		enum { IRQ = Genode::Board_base::TTC0_IRQ_0};
@@ -35,13 +69,70 @@ class Platform_timer_base :
 		/**
 		 * Constructor
 		 */
-		Platform_timer_base() :
+		Platform_timer_base()
+		:
 			Io_mem_connection(Genode::Board_base::TTC0_MMIO_BASE,
 			                  Genode::Board_base::TTC0_MMIO_SIZE),
+			Mmio((Genode::addr_t)
+				Genode::env()->rm_session()->attach(dataspace()))
+		{
+			_disable();
 
-			Ttc_base((Genode::addr_t)Genode::env()->rm_session()->attach(dataspace()))
-		{ }
+			/* enable all interrupts */
+			write<Irqen>(~0);
+
+			/* set match registers to 0 */
+			write<Match1>(0);
+			write<Match2>(0);
+			write<Match3>(0);
+		}
+
+		/**
+		 * Count down 'value', raise IRQ output, wrap counter and continue
+		 */
+		void run_and_wrap(unsigned long const tics)
+		{
+			_disable();
+
+			/* configure timer for a one-shot */
+			Control::access_t control = 0;
+			Control::Mode::set(control, Control::Mode::INTERVAL);
+			Control::Decrement::set(control, 1);
+			Control::Wave_en::set(control, 1);
+			write<Control>(control);
+
+			/* load and enable timer */
+			write<Interval>(tics);
+			write<Control::Disable>(0);
+		}
+
+		/**
+		 * Get timer value and corresponding wrapped status of timer
+		 */
+		unsigned long value(bool & wrapped) const
+		{
+			unsigned long v = read<Value>();
+			wrapped = (bool)read<Irq>();
+			return wrapped ? read<Value>() : v;
+		}
+
+		/**
+		 * Translate native timer value to microseconds
+		 */
+		static unsigned long tics_to_us(unsigned long const tics) {
+			return tics / TICS_PER_US; }
+
+		/**
+		 * Translate microseconds to a native timer value
+		 */
+		static unsigned long us_to_tics(unsigned long const us) {
+			return us * TICS_PER_US; }
+
+		/**
+		 * Translate native timer value to microseconds
+		 */
+		unsigned long max_value() const { return (Interval::access_t)~0; }
 };
 
-#endif /* _OS__SRC__DRIVERS__TIMER__HW__ZYNQ__PLATFORM_TIMER_BASE_H_ */
+#endif /* _HW__ZYNQ__PLATFORM_TIMER_BASE_H_ */
 
