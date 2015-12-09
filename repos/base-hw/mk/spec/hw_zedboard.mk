289ef3febba4dcdd77ea8aa16e9e80863c6923e0
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2015-12-09 18:58:55 +0100

    adopt spec changes for parallella and zedboard

--- repos/base-hw/mk/spec/hw_zedboard.mk
+++ repos/base-hw/mk/spec/hw_zedboard.mk
@@ -6,7 +6,7 @@
 #
 
 # denote which specs are also fulfilled by this spec
-SPECS += hw platform_zedboard
+SPECS += hw zedboard
 
 # configure multiprocessor mode
 NR_OF_CPUS = 1
@@ -15,5 +15,5 @@ NR_OF_CPUS = 1
 LD_TEXT_ADDR ?= 0x00100000
 
 # include implied specs
-include $(call select_from_repositories,mk/spec-hw.mk)
-include $(call select_from_repositories,mk/spec-platform_zedboard.mk)
+include $(call select_from_repositories,mk/spec/hw.mk)
+include $(call select_from_repositories,mk/spec/zedboard.mk)
--- repos/base/include/spec/parallella/drivers/board_base.h
+++ repos/base/include/spec/parallella/drivers/board_base.h
@@ -1,5 +1,5 @@
 /*
- * \brief  Base driver for the Zynq (QEMU)
+ * \brief  Base driver for Parallella Board
  * \author Johannes Schlatow
  * \date   2015-06-30
  */
@@ -11,28 +11,27 @@
  * under the terms of the GNU General Public License version 2.
  */
 
-#ifndef _INCLUDE__PLATFORM__DRIVERS__BOARD_BASE_H_
-#define _INCLUDE__PLATFORM__DRIVERS__BOARD_BASE_H_
+#ifndef _INCLUDE__PARALLELLA__DRIVERS__BOARD_BASE_H_
+#define _INCLUDE__PARALLELLA__DRIVERS__BOARD_BASE_H_
 
-#include <zynq/drivers/board_base.h>
+#include <spec/zynq/drivers/board_base_support.h>
 
 namespace Genode { struct Board_base; }
 
 /**
- * Base driver for the Zynq platform
+ * Base driver for the Parallella platform
  */
 struct Genode::Board_base : Zynq::Board_base
 {
 	enum
 	{
 		/* clocks (assuming 6:2:1 mode) */
-		CPU_1X_CLOCK   = 111111115,
-		CPU_6X4X_CLOCK = 6 * CPU_1X_CLOCK,
+		CPU_1X_CLOCK   = 133000000,
+		CPU_6X4X_CLOCK = 6*CPU_1X_CLOCK,
 
-		/* CPU */
 		CORTEX_A9_CLOCK             = CPU_6X4X_CLOCK,
 		CORTEX_A9_PRIVATE_TIMER_CLK = CORTEX_A9_CLOCK,
 	};
 };
 
-#endif /* _INCLUDE__PLATFORM__DRIVERS__BOARD_BASE_H_ */
+#endif /* _INCLUDE__PARALLELLA__DRIVERS__BOARD_BASE_H_ */
