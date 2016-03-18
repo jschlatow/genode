1f640c81e6aa01b5f17757fea376e2097f22d513
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2016-03-18 21:33:10 +0100

    libports: remove ports present in Genode world

--- repos/libports/lib/import/import-lemon.mk
+++
@@ -1,2 +0,0 @@
-INC_DIR += $(call select_from_ports,lemon)/include
-REP_INC_DIR += src/lib/lemon
--- repos/libports/src/test/soplex/target.mk
+++
@@ -1,6 +0,0 @@
-TARGET   = test-soplex
-LIBS     = soplex stdcxx
-SOPLEX   = $(call select_from_ports,soplex)/src/lib/soplex/src/
-SRC_CC   = example.cpp
-
-vpath example.cpp $(SOPLEX)
