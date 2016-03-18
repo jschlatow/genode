968884ec81537f41bafba88dca0c38853279b9ce
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2016-03-18 21:11:17 +0100

    libports: move lemon and glucose to Genode world repository

--- repos/libports/lib/import/import-glucose.mk
+++
@@ -1,2 +0,0 @@
-INC_DIR += $(call select_from_ports,glucose)/include/glucose
-INC_DIR += $(call select_from_ports,glucose)/include/
--- repos/libports/src/test/lemon_lgf/target.mk
+++
@@ -1,6 +0,0 @@
-TARGET   = test-lemon_lgf
-LIBS     = lemon stdcxx 
-LEMON    = $(call select_from_ports,lemon)/src/lib/lemon/demo
-SRC_CC   = lgf_demo.cc
-
-vpath lgf_demo.cc $(LEMON)
