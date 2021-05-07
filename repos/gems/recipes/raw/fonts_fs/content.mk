2149d669a7bdcb6a7ddb5e1c73083b3e37c8c529
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2021-05-07 11:33:59 +0200

    sculpt: adapt fonts_fs to default font names
    genodelabs/genode#4135

--- repos/gems/recipes/raw/fonts_fs/content.mk
+++ repos/gems/recipes/raw/fonts_fs/content.mk
@@ -1,4 +1,6 @@
-content: fonts_fs.config
+PORT_DIR := $(call port_dir,$(GENODE_DIR)/repos/libports/ports/ttf-bitstream-vera)
 
-fonts_fs.config:
-	cp $(REP_DIR)/recipes/raw/fonts_fs/$@ $@
+content:
+	cp $(PORT_DIR)/ttf/bitstream-vera/COPYRIGHT.TXT LICENSE.bitstream-vera
+	cp $(PORT_DIR)/ttf/bitstream-vera/Vera.ttf ./default.ttf
+	cp $(PORT_DIR)/ttf/bitstream-vera/VeraMono.ttf ./monospace.ttf
