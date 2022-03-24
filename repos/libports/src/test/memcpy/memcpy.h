5a65f9241e56661da8348919242cbb5b0efe8e52
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2022-03-24 11:34:52 +0100

    test/memcpy: fix optimistic results on Linux
    When executed on Linux, the test was impaired by the copy-on-write
    optimisation since the source buffer was never initialised. By default,
    Linux only maps a zeroed page until the first write access to the page
    occurs. Since the source buffer was never written, the corresponding
    page was always present in the physically-indexed data cache. In
    consequence, the test merely measured write performance (similar to memset).

--- repos/libports/src/test/memcpy/memcpy.h
+++ repos/libports/src/test/memcpy/memcpy.h
@@ -17,12 +17,19 @@ void memcpy_test(void * dst = nullptr, void * src = nullptr,
 	void * const to_buf   = dst ? dst : malloc(size);
 
 	Genode::log(from_buf, " -> ", to_buf);
+	/**
+	 * initialising the buffer (with any value) is necessary to
+	 * a) circumvent copy-on-write optimisation on linux and
+	 * b) all pages are already allocated and mapped
+	 */
+	memset(from_buf, 0, size);
+	memset(to_buf,   0, size);
 
 	Test test;
 	test.start();
 
 	for (unsigned i = 0; i < ITERATION; i++)
-		test.copy(to_buf, from_buf, BUF_SIZE);
+		test.copy(to_buf, from_buf, size);
 
 	test.finished();
 
