cb193e51bb7d9f0212df3cb9f58aa9110c693cea
Christian Helmuth <christian.helmuth@genode-labs.com>
2024-01-10 11:41:53 +0100

    WIP btacpi

--- repos/libports/src/app/acpica/btacpi.h
+++ repos/libports/src/app/acpica/btacpi.h
@@ -84,79 +84,223 @@ struct Device
 };
 
 
-ACPI_STATUS detect_resources(ACPI_RESOURCE *resource, void *context)
+struct Resource : ACPI_RESOURCE
 {
-	Device const &dev = *(Device const *)context;
+	struct Irq;
+	struct Extended_irq;
+	struct Gpio;
 
-	switch (resource->Type) {
-	case ACPI_RESOURCE_TYPE_IRQ:
-		log(" ", Right_aligned(8, dev.hid()),
-		    " ", Right_aligned(8, dev.cid()),
-		    " IRQ",
-		    " n=", resource->Data.Irq.InterruptCount,
-		    " first=", resource->Data.Irq.Interrupts[0]);
-		break;
+	static char const * producer_consumer(UINT8 v)
+	{
+		switch (v) {
+		case ACPI_PRODUCER: return "producer";
+		case ACPI_CONSUMER: return "consumer";
+		default: return "?";
+		}
+	}
 
-	case ACPI_RESOURCE_TYPE_EXTENDED_IRQ:
-		log(" ", Right_aligned(8, dev.hid()),
-		    " ", Right_aligned(8, dev.cid()),
-		    " EXTIRQ",
-		    " n=", resource->Data.ExtendedIrq.InterruptCount,
-		    " first=", resource->Data.ExtendedIrq.Interrupts[0]);
-		break;
+	static char const * triggering(UINT8 v)
+	{
+		switch (v) {
+		case ACPI_LEVEL_SENSITIVE: return "level";
+		case ACPI_EDGE_SENSITIVE:  return "edge";
+		default: return "?";
+		}
+	}
 
-	case ACPI_RESOURCE_TYPE_SERIAL_BUS:
-		switch (resource->Data.CommonSerialBus.Type) {
-		case ACPI_RESOURCE_SERIAL_TYPE_I2C:
-			log(" ", Right_aligned(8, dev.hid()),
-			    " ", Right_aligned(8, dev.cid()),
-			    " I2C",
-			    " (", resource->Length, ")",
-			    " type=", resource->Data.CommonSerialBus.Type,
-			    " length=", resource->Data.CommonSerialBus.TypeDataLength,
-			    " vlength=", resource->Data.CommonSerialBus.VendorLength,
-			    " ", (char const *)resource->Data.CommonSerialBus.ResourceSource.StringPtr);
-			break;
-		case ACPI_RESOURCE_SERIAL_TYPE_SPI:
-			log(" ", Right_aligned(8, dev.hid()),
-			    " ", Right_aligned(8, dev.cid()),
-			    " SPI",
-			    " (", resource->Length, ")",
-			    " type=", resource->Data.CommonSerialBus.Type,
-			    " length=", resource->Data.CommonSerialBus.TypeDataLength,
-			    " vlength=", resource->Data.CommonSerialBus.VendorLength,
-			    " ", (char const *)resource->Data.CommonSerialBus.ResourceSource.StringPtr);
-			break;
-		case ACPI_RESOURCE_SERIAL_TYPE_UART:
-			log(" ", Right_aligned(8, dev.hid()),
-			    " ", Right_aligned(8, dev.cid()),
-			    " UART",
-			    " (", resource->Length, ")",
-			    " type=", resource->Data.CommonSerialBus.Type,
-			    " length=", resource->Data.CommonSerialBus.TypeDataLength,
-			    " vlength=", resource->Data.CommonSerialBus.VendorLength,
-			    " ", (char const *)resource->Data.CommonSerialBus.ResourceSource.StringPtr);
-			break;
-		} break;
+	static char const * polarity(UINT8 v)
+	{
+		switch (v) {
+		case ACPI_ACTIVE_HIGH: return "high";
+		case ACPI_ACTIVE_LOW:  return "low";
+		case ACPI_ACTIVE_BOTH: return "both";
+		default: return "?";
+		}
+	}
 
-	case ACPI_RESOURCE_TYPE_GPIO:
-		log(" ", Right_aligned(8, dev.hid()),
-		    " ", Right_aligned(8, dev.cid()),
-		    " GPIO",
-		    " type=", resource->Data.Gpio.ConnectionType ? "IO" : "INT",
-		    " pintablelength=", resource->Data.Gpio.PinTableLength,
-		    " pintable[0]=", resource->Data.Gpio.PinTable[0]);
-//		    " ", (char const *)resource->Data.CommonSerialBus.ResourceSource.StringPtr);
-		break;
+	static char const * sharable(UINT8 v)
+	{
+		switch (v) {
+		case ACPI_EXCLUSIVE: return "exclusive";
+		case ACPI_SHARED:    return "shared";
+		default: return "?";
+		}
+	}
+
+	/*
+	 * FIXME
+	 *
+	 * - Length is dynamic and may include lists
+	 * - better use AcpiGetCurrentResources() into buffer owned by Device
+	 * - parse via AcpiWalkResourceBuffer()
+	 * - then all references from Resource and sub classes share a life-time
+	 *   with Device
+	 */
+	Resource(ACPI_RESOURCE const &res) : ACPI_RESOURCE(res) { }
+
+	void print(Genode::Output &out) const;
+};
+
+
+struct Resource::Irq : ACPI_RESOURCE_IRQ
+{
+	Irq(ACPI_RESOURCE_IRQ const &res) : ACPI_RESOURCE_IRQ(res)
+	{
+		if (InterruptCount > 1)
+			warning("Resource::Irq: only first of ", InterruptCount, " IRQs supported");
+	}
+
+	void print(Genode::Output &out) const
+	{
+		using Genode::print;
+
+		print(out, "IRQ");
+		print(out, " ", Interrupts[0]);
+		print(out, " ", triggering(Triggering));
+		print(out, " ", polarity(Polarity));
+		print(out, " ", sharable(Sharable));
+	}
+};
+
+
+struct Resource::Extended_irq : ACPI_RESOURCE_EXTENDED_IRQ
+{
+	Extended_irq(ACPI_RESOURCE_EXTENDED_IRQ const &res) : ACPI_RESOURCE_EXTENDED_IRQ(res)
+	{
+		if (InterruptCount > 1)
+			warning("Resource::Extended_irq: only first of ", InterruptCount, " IRQs supported");
+	}
+
+	void print(Genode::Output &out) const
+	{
+		using Genode::print;
+
+		print(out, "EXTIRQ");
+		print(out, " ", Interrupts[0]);
+		print(out, " ", triggering(Triggering));
+		print(out, " ", polarity(Polarity));
+		print(out, " ", sharable(Sharable));
+		print(out, " ", producer_consumer(ProducerConsumer));
+
+		/* FIXME StringPtr is outside ACPI_RESOURCE_EXTENDED_IRQ */
+		if (ResourceSource.StringPtr)
+			print(out, " ", (char const *)ResourceSource.StringPtr);
+	}
+};
+
+
+struct Resource::Gpio : ACPI_RESOURCE_GPIO
+{
+	Gpio(ACPI_RESOURCE_GPIO const &res) : ACPI_RESOURCE_GPIO(res)
+	{
+		/* *PinTable of PinTableLength */
+		/* *VendorData of VendorLength */
+	}
+
+	static char const * connection_type(UINT8 v)
+	{
+		switch (v) {
+		case ACPI_RESOURCE_GPIO_TYPE_INT: return "int";
+		case ACPI_RESOURCE_GPIO_TYPE_IO:  return "io";
+		default: return "?";
+		}
+	}
+
+	static char const * pin_config(UINT8 v)
+	{
+		switch (v) {
+		case ACPI_PIN_CONFIG_DEFAULT:  return "default";
+		case ACPI_PIN_CONFIG_PULLUP:   return "pullup";
+		case ACPI_PIN_CONFIG_PULLDOWN: return "pulldown";
+		case ACPI_PIN_CONFIG_NOPULL:   return "nopull";
+		default: return "?";
+		}
+	}
+
+	void print(Genode::Output &out) const
+	{
+		using Genode::print;
+
+		print(out, "GPIO");
+		print(out, " ", connection_type(ConnectionType));
+		print(out, " ", pin_config(PinConfig));
+		print(out, " ", triggering(Triggering));
+		print(out, " ", polarity(Polarity));
+		print(out, " ", sharable(Sharable));
+		print(out, " ", producer_consumer(ProducerConsumer));
+	}
+};
+
+
+void Resource::print(Genode::Output &out) const
+{
+	using Genode::print;
+
+	switch (Type) {
+	case ACPI_RESOURCE_TYPE_IRQ:          print(out, Irq(Data.Irq)); break;
+	case ACPI_RESOURCE_TYPE_EXTENDED_IRQ: print(out, Extended_irq(Data.ExtendedIrq)); break;
+
+//	case ACPI_RESOURCE_TYPE_SERIAL_BUS:
+//		switch (resource->Data.CommonSerialBus.Type) {
+//		case ACPI_RESOURCE_SERIAL_TYPE_I2C:
+//			log(" ", Right_aligned(8, dev.hid()),
+//			    " ", Right_aligned(8, dev.cid()),
+//			    " I2C",
+//			    " (", resource->Length, ")",
+//			    " type=", resource->Data.CommonSerialBus.Type,
+//			    " length=", resource->Data.CommonSerialBus.TypeDataLength,
+//			    " vlength=", resource->Data.CommonSerialBus.VendorLength,
+//			    " ", (char const *)resource->Data.CommonSerialBus.ResourceSource.StringPtr);
+//			break;
+//		case ACPI_RESOURCE_SERIAL_TYPE_SPI:
+//			log(" ", Right_aligned(8, dev.hid()),
+//			    " ", Right_aligned(8, dev.cid()),
+//			    " SPI",
+//			    " (", resource->Length, ")",
+//			    " type=", resource->Data.CommonSerialBus.Type,
+//			    " length=", resource->Data.CommonSerialBus.TypeDataLength,
+//			    " vlength=", resource->Data.CommonSerialBus.VendorLength,
+//			    " ", (char const *)resource->Data.CommonSerialBus.ResourceSource.StringPtr);
+//			break;
+//		case ACPI_RESOURCE_SERIAL_TYPE_UART:
+//			log(" ", Right_aligned(8, dev.hid()),
+//			    " ", Right_aligned(8, dev.cid()),
+//			    " UART",
+//			    " (", resource->Length, ")",
+//			    " type=", resource->Data.CommonSerialBus.Type,
+//			    " length=", resource->Data.CommonSerialBus.TypeDataLength,
+//			    " vlength=", resource->Data.CommonSerialBus.VendorLength,
+//			    " ", (char const *)resource->Data.CommonSerialBus.ResourceSource.StringPtr);
+//			break;
+//		} break;
+//
+//	case ACPI_RESOURCE_TYPE_GPIO:
+//		log(" ", Right_aligned(8, dev.hid()),
+//		    " ", Right_aligned(8, dev.cid()),
+//		    " GPIO",
+//		    " type=", resource->Data.Gpio.ConnectionType ? "IO" : "INT",
+//		    " pintablelength=", resource->Data.Gpio.PinTableLength,
+//		    " pintable[0]=", resource->Data.Gpio.PinTable[0]);
+////		    " ", (char const *)resource->Data.CommonSerialBus.ResourceSource.StringPtr);
+//		break;
 
 	default:
-		log(" ", Right_aligned(8, dev.hid()),
-		    " ", Right_aligned(8, dev.cid()),
-		    " RESOURCE TYPE ", resource->Type, " not handled");
+		print(out, "unknown type ", Type);
 		break;
-
-	case ACPI_RESOURCE_TYPE_END_TAG: /* ignore */ break;
 	}
+}
+
+
+ACPI_STATUS detect_resources(ACPI_RESOURCE *resource, void *context)
+{
+	if (resource->Type == ACPI_RESOURCE_TYPE_END_TAG)
+		return AE_OK;
+
+	Device const &dev = *(Device const *)context;
+
+	log(" ", Right_aligned(8, dev.hid()),
+	    " ", Right_aligned(8, dev.cid()),
+	    " ", Resource(*resource));
 
 	return AE_OK;
 }
