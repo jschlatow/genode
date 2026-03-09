2237218876059d2af04035c4a67dc7d4469804f7
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2026-03-09 14:19:14 +0100

    nitpicker: send Focus_leave after transient focus
    When a transiently focused session loses the focus
    at the end of a key sequence, it is only fair
    to submit a Focus_leave event.
    
    genodelabs/genode#5716

--- repos/os/src/server/nitpicker/user_state.cc
+++ repos/os/src/server/nitpicker/user_state.cc
@@ -340,9 +340,12 @@ void User_state::_handle_input_event(Input::Event ev)
 
 		update_hover();
 
-		if (_drag || _global_key_sequence || _transient_focus)
+		if (_drag || _global_key_sequence || _transient_focus) {
 			if (_input_receiver && (_input_receiver != _hovered))
 				_input_receiver->submit_input_event(Hover_leave());
+			if (_input_receiver && (_input_receiver != _focused))
+				_input_receiver->submit_input_event(Focus_leave());
+		}
 
 		_drag = false;
 
