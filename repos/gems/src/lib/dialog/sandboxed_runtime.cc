4967d079d3e15aa703cb6643dd609ff8abe48136
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2026-03-11 16:11:29 +0100

    fixup! lib/dialog: make use of Seq_number_generator
    genodelabs/genode#5716

--- repos/gems/src/lib/dialog/sandboxed_runtime.cc
+++ repos/gems/src/lib/dialog/sandboxed_runtime.cc
@@ -20,34 +20,12 @@
 using namespace Dialog;
 
 
-static bool click(Input::Event const &event)
-{
-	bool result = false;
-
-	if (event.key_press(Input::BTN_LEFT))
-		result = true;
-
-	event.handle_touch([&] (Input::Touch_id id, float, float) {
-		if (id.value == 0)
-			result = true; });
-
-	return result;
-}
+static bool click(Input::Event const &event) {
+	return Input::Seq_number_generator::click(event); }
 
 
-static bool clack(Input::Event const &event)
-{
-	bool result = false;
-
-	if (event.key_release(Input::BTN_LEFT))
-		result = true;
-
-	event.handle_touch_release([&] (Input::Touch_id id) {
-		if (id.value == 0)
-			result = true; });
-
-	return result;
-}
+static bool clack(Input::Event const &event) {
+	return Input::Seq_number_generator::clack(event); }
 
 
 struct Sandboxed_runtime::Gui_session : Session_object<Gui::Session>
